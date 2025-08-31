e0911185c7c83726ba64ac30c2a1ae494321fda7
Shubham Kumar Mandal <shubhampaul756@gmail.com>
2025-09-01 02:34:43 +0530

    cors added

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://ai-website-builder-n763.vercel.app'],
   credentials: true
 }));
 
